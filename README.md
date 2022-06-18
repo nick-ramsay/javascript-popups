a52e98aee509d8e30f7f54d372aafbfce0eaae61
Nick Ramsay <NickRamsay1989@gmail.com>
2022-06-18 21:11:04 +1000

    Creating clean version with alerts and confirm options added

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# browser-prompts
+# javascript-popups
+
+See this source for popups: https://www.w3schools.com/js/js_popup.asp
--- javascript/prompts.js
+++ javascript/prompts.js
@@ -8,37 +8,40 @@ const renderName = (name) => {
         document.getElementById("update-name-btn").style.display = "inline";
         document.getElementById("delete-name-btn").style.display = "inline"
         document.getElementById("enter-name-btn").style.display = "none"
-        
+
         document.getElementById("your-name").innerHTML = "&#128515; Welcome, " + name + "! &#128515;"
     }
 }
 
 const initialNameCheck = () => {
     document.getElementById("update-name-btn").style.display = "none";
-        document.getElementById("enter-name-btn").style.display = "none"
-        document.getElementById("delete-name-btn").style.display = "none"
+    document.getElementById("enter-name-btn").style.display = "none"
+    document.getElementById("delete-name-btn").style.display = "none"
     let yourName = localStorage.getItem("your_name");
 
     if (yourName === null || yourName === "" || yourName === "null") {
-        localStorage.setItem("your_name",prompt("Please enter your preferred name"));
+        localStorage.setItem("your_name", prompt("Please enter your preferred name"));
         renderName(localStorage.getItem("your_name"));
     } else {
         renderName(yourName)
     }
 
-    
 }
 
 const deleteName = () => {
-   localStorage.removeItem("your_name");
-   renderName(localStorage.getItem("your_name"));
+    localStorage.removeItem("your_name");
+    renderName(localStorage.getItem("your_name"));
+    alert("Your name has been deleted 👍");
 };
 
 const updateName = () => {
     let currentName = localStorage.getItem("your_name");
-    let revisedName = prompt("Edit your name below", currentName);
-    localStorage.setItem("your_name", revisedName);
-    renderName(localStorage.getItem("your_name"));
+    let proceed = confirm("Are you sure you'd like to change your name?");
+    if (proceed) {
+        let revisedName = prompt("Edit your name below", currentName);
+        revisedName != null ? localStorage.setItem("your_name", revisedName):"";
+        renderName(localStorage.getItem("your_name"));
+    }
 }
 
 document.getElementById("delete-name-btn").addEventListener("click", deleteName);
